ace186cce039f42cc46bfbf0b7ba5ba55dab1270
Juan Vidal <juanmvg2003@gmail.com>
2024-09-17 09:20:43 -0500

    cors config

--- Back_End/index.js
+++ Back_End/index.js
@@ -1,8 +1,16 @@
 import express from 'express'
+import cors from "cors"
 import { dbConnection } from './src/DB/dbConnection.js'
 
 const app = express()
 app.use(express.json())
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    methods: ['GET', 'POST', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    optionsSuccessStatus: 200,
+}));
 
 
 import routesPartida from './src/routes/partida.js'
